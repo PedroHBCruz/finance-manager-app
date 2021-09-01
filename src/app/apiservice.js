fd3ed338cfc0dffab6b4802ddd3079fe55b90434
PedroHBCruz <75803558+PedroHBCruz@users.noreply.github.com>
2021-09-01 15:34:47 -0400

    Separando a lógica de acesso ás APIs dos componentes

---
+++ src/app/apiservice.js
@@ -0,0 +1,30 @@
+import axios from "axios"
+
+const httpCLient = axios.create({
+    baseURL: 'http://localhost:8080'
+})
+
+class ApiService {
+
+    constructor(apiurl) {
+        this.apiurl = apiurl;-
+    }
+
+    post(url, objeto){
+        return httpCLient.post(url, objeto);
+    }
+
+    put(url, objeto){
+        return httpCLient.put(url, objeto);
+    }
+
+    delete(url){
+        return httpCLient.delete(url);
+    }
+
+    get(url){
+        return httpCLient.get(url);
+    }
+
+}
+export default ApiService
